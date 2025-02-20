7dc856d8f533b49fb44d42894c589067002393c0
kapris <kaprisdimitri@gmail.com>
2025-02-20 20:07:49 +0100

    New Spawn Algo

--- Assets/Developement/Scripts/Player/PlayerInstanceur.cs
+++ Assets/Developement/Scripts/Player/PlayerInstanceur.cs
@@ -13,7 +13,8 @@ public class PlayerInstanceur : MonoBehaviour
 
     [SerializeField] private Transform playerParent;
 
-    [SerializeField] private List<Transform> sps;
+    [SerializeField] private List<Transform> spawnPoints;
+    private List<Transform> _spawnPointsUse;
 
     public UnityEvent<PlayerData> OnPlayerSpawn;
     public UnityEvent<PlayerData> OnPlayerDespawn;
@@ -34,7 +35,7 @@ public class PlayerInstanceur : MonoBehaviour
                 playerSkin = defaultSkin;
             } 
 
-            gameData.playersData[i].playerObject = Instantiate(playerPrefab, sps[Random.Range(0,sps.Count)].position, Quaternion.identity, playerParent);
+            gameData.playersData[i].playerObject = Instantiate(playerPrefab, ChoseSpawnPosition(), Quaternion.identity, playerParent);
 
             gameData.playersData[i].playerObject.GetComponent<PlayerController>().InitializePlayer(new PlayerController.InitializeData()
             {
@@ -44,6 +45,30 @@ public class PlayerInstanceur : MonoBehaviour
 
             OnPlayerSpawn.Invoke(gameData.playersData[i]);
         }
+    }
+
+    private Vector3 ChoseSpawnPosition ()
+    {
+        if(spawnPoints.Count == 0)
+        {
+            if(_spawnPointsUse == null || _spawnPointsUse.Count == 0)
+            {
+                return Vector3.zero;
+            }
+
+            for(int i = 0; i < _spawnPointsUse.Count; i+=0)
+            {
+                spawnPoints.Add(_spawnPointsUse[i]);
+                _spawnPointsUse.Remove(_spawnPointsUse[i]);
+            }
+        }
+
+        Transform transform = spawnPoints[Random.Range(0, spawnPoints.Count)];
+
+        spawnPoints.Remove(transform);
+        _spawnPointsUse.Add(transform);
+
+        return transform.position;
     }
 
     public void InstancePlayer (PlayerData playerData)
@@ -57,7 +82,7 @@ public class PlayerInstanceur : MonoBehaviour
             playerSkin = defaultSkin;
         }
 
-        playerData.playerObject = Instantiate(playerPrefab, sps[Random.Range(0, sps.Count)].position, Quaternion.identity, playerParent);
+        playerData.playerObject = Instantiate(playerPrefab, ChoseSpawnPosition(), Quaternion.identity, playerParent);
 
         playerData.playerObject.GetComponent<PlayerController>().InitializePlayer(new PlayerController.InitializeData()
         {
